3f55dcee4abac383b485dfbc9cba7d21599e01f3
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-04-09 16:22:36 -0300

    URL_BASE changed to deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const URL_BASE = "http://localhost:3001";
-// export const URL_BASE = "https://challenge-n365-server.onrender.com"
+// export const URL_BASE = "http://localhost:3001";
+export const URL_BASE = "https://challenge-n365-server.onrender.com";
